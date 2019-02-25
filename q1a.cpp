ba710c8f0d01e4876fab18b7d8d7e41f56d21f59
acizas <38055413+acizas@users.noreply.github.com>
2019-02-25 13:53:44 -0800

    Add files via upload

---
+++ q1a.cpp
@@ -0,0 +1,2 @@
+
+// The program is trying to access index 374 of the pointer to the array[1024], which has been made null.
---
+++ q1b.cpp
@@ -0,0 +1,2 @@
+
+// The program is trying to store the value 234 when a segmentation fault occurs.
